2a740f1546d28c692dbbad839d2ff3f1b53a908c
art-idea5642 <chertezhy.ru@gmail.com>
2024-06-01 20:06:31 +0400

    Initial commit

--- src/main/java/com/art/calculator/controller/CalculatorController.java
+++ src/main/java/com/art/calculator/controller/CalculatorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/calculator")
+@RequestMapping("/calculator")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
@@ -20,24 +20,40 @@ public class CalculatorController {
         return "Добро пожаловать в калькулятор";
     }
 
-    @GetMapping
-    public String sum(int num1, int num2) {
-        return num1+ " + " + num2 + " = " + calculatorService.sum(num1, num2);
+    @GetMapping("/sum")
+    public String sum(Integer num1, Integer num2) {
+        if (num1==null || num2 == null){
+            return "Параметр не задан";
+        }
+        return num1 + " + " + num2 + " = " + calculatorService.sum(num1, num2);
     }
 
-    @GetMapping
-    public String minus(int num1, int num2) {
-        return num1+ " - " + num2 + " = " + calculatorService.minus(num1, num2);
+    @GetMapping("/minus")
+    public String minus(Integer num1, Integer num2) {
+        if (num1==null || num2 == null){
+            return "Параметр не задан";
+        }
+        return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
     }
 
-    @GetMapping
-    public String multiply(int num1, int num2) {
-        return num1+ " * " + num2 + " = " + calculatorService.multiply(num1, num2);
+    @GetMapping("/multiply")
+    public String multiply(Integer num1, Integer num2) {
+        if (num1==null || num2 == null){
+            return "Параметр не задан";
+        }
+        return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
     }
 
-    @GetMapping
-    public String divide(int num1, int num2) {
-        return num1+ " / " + num2 + " = " + calculatorService.divide(num1, num2);
+    @GetMapping("/divide")
+    public String divide(Integer num1, Integer num2) {
+        if (num1==null || num2 == null){
+            return "Параметр не задан";
+        }
+        if (num2 == 0) {
+            return "Делить на ноль нельзя!";
+        }
+        return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
+
     }
 
 }
